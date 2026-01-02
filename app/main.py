d3995e2abed1a0c26b554b7c647e8d2f667043a0
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2026-01-03 01:35:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from .database import engine
 from . import models
@@ -8,6 +9,14 @@ from .routers import movies, actors, directors, genres
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Movie App Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
